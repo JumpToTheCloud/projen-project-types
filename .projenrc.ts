85161c05ffea875c79b79902dcc09883e04cefe4
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2025-12-18 19:33:59 +0100

    chore(projen): configured repository

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,90 @@
-import { cdk } from 'projen';
+import { cdk, ReleasableCommits } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
+import { AppPermission } from 'projen/lib/github/workflows-model';
+import {
+  NpmAccess,
+  TrailingComma,
+  UpgradeDependenciesSchedule,
+} from 'projen/lib/javascript';
+import { ReleaseTrigger } from 'projen/lib/release';
+
 const project = new cdk.JsiiProject({
-  author: 'Antonio Márquez Pérez',
+  author: 'Jumpt to the Cloud',
   authorAddress: 'antonio.marquez@jumptothecloud.tech',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.9.0',
   name: 'projen-project-types',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:JumpToTheCloud/projen-project-types.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      trailingComma: TrailingComma.ES5,
+      singleQuote: true,
+      bracketSpacing: true,
+      semi: true,
+    },
+  },
+  autoMerge: false,
+  mergify: false,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {},
+  release: true,
+  releaseTrigger: ReleaseTrigger.workflowDispatch(),
+  releasableCommits: ReleasableCommits.featuresAndFixes(),
+  depsUpgrade: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  majorVersion: 1,
+  prerelease: 'beta',
+  jestOptions: {
+    jestConfig: {
+      verbose: true,
+    },
+  },
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp({
+      permissions: {
+        pullRequests: AppPermission.WRITE,
+        contents: AppPermission.WRITE,
+      },
+    }),
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: [
+          'feat',
+          'fix',
+          'chore',
+          'docs',
+          'style',
+          'refactor',
+          'test',
+          'revert',
+          'ci',
+        ],
+      },
+    },
+  },
+  // deps: [],
+  // description: undefined,
+  devDeps: ['commitizen', 'cz-customizable'],
+  packageName: '@jttc/projen-project-types',
+  npmAccess: NpmAccess.PUBLIC,
 });
+
+project.addTask('commit', {
+  description:
+    'Commit changes with conventional commits prompts provided by Commitizen',
+  steps: [
+    {
+      exec: './node_modules/cz-customizable/standalone.js',
+      receiveArgs: false,
+      say: 'committing changes',
+    },
+  ],
+});
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@jttc/projen-project-types.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@jttc/projen-project-types.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@jttc/projen-project-types'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@jttc/projen-project-types.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@jttc/projen-project-types.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
